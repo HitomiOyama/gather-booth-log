8aab716c4814551d45ce44a98bb9ba11b02942c9
hitomi ooyama <h_ooyama@freestyles.jp>
2023-02-08 16:51:21 +0900

    test

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ globalThis.WebSocket = require("isomorphic-ws");
 const game = new Game(undefined, () => Promise.resolve({apiKey: API_KEY}));
 game.init(SPACE_ID);
 game.connect();
-console.log('test');
+console.log('あああああ');
 
 game.subscribeToConnection(
     (connected) => {
