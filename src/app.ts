6e547e3f59572c6c5100f48ec24303b98067371d
inabayuno <a0619006@iwate-u.ac.jp>
2023-02-17 16:47:02 +0900

    色々変えました

--- src/app.ts
+++ src/app.ts
@@ -8,23 +8,7 @@ game.init(SPACE_ID);
 game.connect();
 console.log('test');
 console.log("hello world");
-
-game.subscribeToConnection(
-    (connected) => {
-        console.log("is connected:", connected);
-        game.subscribeToEvent("error", console.info);
-        game.subscribeToEvent("warn", console.warn);
-        game.subscribeToEvent("error", console.error);
-    }
-)
-
-const atRoomEntrance: string[] = []
-const inRoomX: string[] = []
-
-console.log(atRoomEntrance)
-console.log(inRoomX)
-
-game.subscribeToEvent('playerMoves', () => {
+function moveProgram(){
     const playerIds = Object.keys(game.players)
 
     const playersPosInfo:{[id: string]: string} = {}
@@ -41,16 +25,17 @@ game.subscribeToEvent('playerMoves', () => {
         const player = game.players[playerId]
         if(player.x >= 53 && player.x <=58 && player.y >=17 && player.y <=23) {
             playersPosInfo[playerId] = "roomX"
+            console.log(playersPosInfo)
         }
-     
+    
         console.log("x:" + player.x + " y:" + player.y)
         const conditions1 = (player.x === 55 || player.x === 56)
 
         if(conditions1 && player.y === 24) {
-           playersPosInfo[playerId] = "roomXEntrance"
+        playersPosInfo[playerId] = "roomXEntrance"
 
-           console.log(playersPosInfo)
-           console.log(year + "年" + month + "月" + date + "日" + hour + ":" + min + ":" + sec)
+        console.log(playersPosInfo)
+        console.log(year + "年" + month + "月" + date + "日" + hour + ":" + min + ":" + sec)
 
         // 入室
         } else if(conditions1 && player.y === 23) {
@@ -68,7 +53,26 @@ game.subscribeToEvent('playerMoves', () => {
         }
     }
 
+}
 
+game.subscribeToConnection(
+    (connected) => {
+        console.log("is connected:", connected);
+        game.subscribeToEvent("error", console.info);
+        game.subscribeToEvent("warn", console.warn);
+        game.subscribeToEvent("error", console.error);
+        moveProgram();
+    }
+)
+
+const atRoomEntrance: string[] = []
+const inRoomX: string[] = []
+
+console.log(atRoomEntrance)
+console.log(inRoomX)
+
+game.subscribeToEvent('playerMoves', () => {
+    moveProgram ();
 })
 
 
