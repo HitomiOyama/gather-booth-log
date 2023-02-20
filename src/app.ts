9bf6b8530783c129475ed18b1fd1dd5feef32a93
hitomi ooyama <h_ooyama@freestyles.jp>
2023-02-20 15:39:17 +0900

    メソッド化

--- src/app.ts
+++ src/app.ts
@@ -1,64 +1,136 @@
-import {Game, GameMap} from "@gathertown/gather-game-client";
-import {SPACE_ID, API_KEY} from "./config";
+import {Game, Player} from "@gathertown/gather-game-client";
+import {SPACE_ID, API_KEY } from "./config";
+import {GoogleSpreadsheetService} from "./googleSpreadSheet"
 
 globalThis.WebSocket = require("isomorphic-ws");
 
 const game = new Game(undefined, () => Promise.resolve({apiKey: API_KEY}));
 game.init(SPACE_ID);
 game.connect();
-console.log('あああああ');
-console.log('test');
-console.log("world");
 
+const playerIds = Object.keys(game.players)
+console.log("playerIds are " + Object.keys(game.players))
+
+const playersPosInfo:{[id: string]: string} = {}
+let spreadService: GoogleSpreadsheetService;
+
+/**
+ * Gatherへの接続
+ */
 game.subscribeToConnection(
-    (connected) => {
+    async (connected) => {
         console.log("is connected:", connected);
         game.subscribeToEvent("error", console.info);
         game.subscribeToEvent("warn", console.warn);
         game.subscribeToEvent("error", console.error);
-    }
+
+        // スプレッドシート書き込みを行うインスタンスの生成
+        spreadService = await GoogleSpreadsheetService.getInstance();
+    }   
 )
 
-const atRoomEntrance: string[] = []
-const inRoomX: string[] = []
-
-console.log(atRoomEntrance)
-console.log(inRoomX)
-
+/**
+ * プレイヤーの移動を検知して入退室を記録する
+ */
 game.subscribeToEvent('playerMoves', () => {
     const playerIds = Object.keys(game.players)
-
-    const playersPosInfo:{[id: string]: string} = {}
-
+    
     for(const playerId of playerIds) {
-        const player = game.players[playerId]
-        if(player.x >= 53 && player.x <=58 && player.y >=17 && player.y <=23) {
-            playersPosInfo[playerId] = "roomX"
-        }
-     
+        const player = game.players[playerId] 
         console.log("x:" + player.x + " y:" + player.y)
-        const conditions1 = (player.x === 55 || player.x === 56)
 
-        if(conditions1 && player.y === 24) {
-           playersPosInfo[playerId] = "roomXEntrance"
+        const room1AreaX = generateCoordinate(53, 58, player.x)
+        const room1AreaY = generateCoordinate(17, 23, player.y)
+        const room1EntranceX = generateEntranceCoordinate(55, player.x, 56)
+        const room1EntranceY = generateEntranceCoordinate(24, player.y)
 
-           console.log(playersPosInfo)
+        const room2AreaX = generateCoordinate(60, 65, player.x)
+        const room2AreaY = generateCoordinate(17, 23, player.y)
+        const room2EntranceX = generateEntranceCoordinate(62, player.x, 63)
+        const room2EntranceY = generateEntranceCoordinate(24, player.y)
+ 
+        // room1入室
+        isInRoom(room1AreaX, room1AreaY, playerId, "room1")
 
-        // 入室
-        } else if(conditions1 && player.y === 23) {
-            playersPosInfo[playerId] = "inRoomX"
+        // room2退室
+        isAtEntrance(room1EntranceX, room1EntranceY, playerId)
 
-            console.log(playersPosInfo)
+        // room2入室
+        isInRoom(room2AreaX, room2AreaY, playerId, "room2")
 
-        // 退室
-        } else if (conditions1 && player.y === 25) {
-            playersPosInfo[playerId] = "outside"
-
-            console.log(playersPosInfo)
-        }
+        // room2退室
+        isAtEntrance(room2EntranceX, room2EntranceY, playerId)
     }
-
-
 })
 
 
+
+/**
+ * スプレッドシートへの書き込み
+ * @param {string} name プレイヤー名
+ * @param {string} room 部屋名
+ */
+const writeIntoSpreadSheet = async (name: string, room: string) => {
+    
+    await spreadService.addRow('シート1', {name: name, room: room, time: Date()}); // ヘッダ似合うような形で行を追加
+}
+
+/**
+ * 入室を検知しますす
+ * @param {boolean} conditionX x軸についての条件
+ * @param {boolean} conditionY y軸についての条件
+ * @param {string} playerId プレイヤーのID
+ * @param {string} roomName 部屋の名前
+ * 
+ */
+const isInRoom = (condition1: boolean, condition2: boolean, playerId: string, roomName: string) => {
+    const player = game.players[playerId]
+    if( condition1 && condition2 ) { 
+        if(playersPosInfo[playerId] !== roomName) {
+            playersPosInfo[playerId] = roomName
+            writeIntoSpreadSheet(player.name,playersPosInfo[playerId]);
+        }    
+    } 
+}
+
+/**
+ * 退室を検知します
+ * @param {boolean} conditionX x軸についての条件
+ * @param {boolean} conditionY y軸についての条件
+ * @param {string} playerId プレイヤーのID
+ */
+const isAtEntrance = (conditionX: boolean, conditionY: boolean, playerId: string) => {
+    const player = game.players[playerId]
+    if (conditionX && conditionY ) {
+        if(playersPosInfo[playerId] !== "outside") {
+            playersPosInfo[playerId] = "outside"
+            writeIntoSpreadSheet(player.name,playersPosInfo[playerId]);
+        }
+    }
+}
+
+
+/**
+ * 入室の条件（部屋の範囲）を生成します
+ * @param {number} min 最小値
+ * @param {number} max 最大値
+ * @param {number} player プレイヤーの現在地
+ */
+const generateCoordinate = (min: number, max: number, player: number) => {
+    return player >= min && player <=max
+}
+
+
+/**
+ * 退室の条件(入口のます)を生成します
+ * @param {number} min 最小値
+ * @param {number} player プレイヤーの現在地
+ * @param {number} max 最大値
+ */
+const generateEntranceCoordinate = (min: number, player: number, max?: number)=> {
+    if(max) {
+        return player === min || player === max
+    }
+    
+    return player === min
+}
